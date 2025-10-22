c594095a540ae96c6399b7c202052ed11e2a3ccb
Lucas Nogueira de Souza <lukanog@hotmail.com>
2025-10-21 22:59:24 -0300

    feat: change  wflucasnogueira.py

--- wflucasnogueira.py
+++ wflucasnogueira.py
@@ -1,1 +1,31 @@
-print("Jesus saves.")
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+def gerar_documento():
+    return {'compras': 100, 'artefatos': 100}
+
+def somar_parametros(**context):
+    documento = context['ti'].xcom_pull(task_ids='gerar_doc')
+    soma = documento['compras'] + documento['artefatos']
+    print(soma)
+
+with DAG(
+    'lucas_nogueira_wf',
+    default_args={'retries': 2},
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False
+) as dag:
+
+    tarefa_gerar_documento = PythonOperator(
+        task_id='gerar_doc',
+        python_callable=gerar_documento
+    )
+
+    tarefa_somar_parametros = PythonOperator(
+        task_id='somar_params',
+        python_callable=somar_parametros
+    )
+
+    tarefa_gerar_documento >> tarefa_somar_parametros
